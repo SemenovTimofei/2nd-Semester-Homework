ea0275c84259eb9beee1525f96b57765b1636a4a
SemenovTimofei <st117342@student.spbu.ru>
2024-02-27 16:16:15 +0300

    Add initial user interaction, add main BWT functions

---
+++ Барроуз-Уилер/BWT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BWT
+{
+    public static int CompareShifts(string input, int index1, int index2)
+    {
+        string shift1 = input.Substring(index1) + input.Substring(0, index1);
+        string shift2 = input.Substring(index2) + input.Substring(0, index2);
+
+        return String.Compare(shift1, shift2);
+    }
+
+    public static (string, int) BWTransform(string input)
+    {
+        var suffixArray = Enumerable.Range(0, input.Length).ToArray();
+        Array.Sort(suffixArray, (index1, index2) => BWT.CompareShifts(input, index1, index2));
+
+        for (int i = 0; i < suffixArray.Length; ++i)
+        {
+            suffixArray[i] = i;
+        }
+
+        Array.Sort(suffixArray, (index1, index2) => BWT.CompareShifts(input, index1, index2));
+
+        char[] resultArray = new char[input.Length];
+        for (int i = 0; i < input.Length; ++i)
+        {
+            resultArray[i] = suffixArray[i] > 0 ? input[suffixArray[i] - 1] : input[input.Length - 1];
+        }
+
+        int endPosition = Array.IndexOf(suffixArray, 0);
+
+        return (new string(resultArray), endPosition);
+    }
+}
--- Барроуз-Уилер/Program.cs
+++ Барроуз-Уилер/Program.cs
@@ -4,14 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Барроуз_Уилер
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("0 - Exit\n" +
+            "1 - Burrows-Wheeler Transformation\n" +
+            "2 - Reverse Burrows-Wheeler Transformation\n");
+        Console.WriteLine("Enter command: ");
+
+        int command = int.Parse(Console.ReadLine());
+
+        while (command != 0)
         {
-            string text = "example";
-            Console.WriteLine(text);
+            switch (command)
+            {
+                case 0:
+                    continue;
+                case 1:
+                    Console.WriteLine("Enter a string: ");
+                    var text = Console.ReadLine();
+                    // var text = "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES";
+
+                    (string result, int index) = BWT.BWTransform(text);
+                    Console.WriteLine("The result: {0}", result);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter a string: ");
+                    text = Console.ReadLine();
+                    Console.WriteLine("Enter position: ");
+                    int position = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("The result: {0} {1}", text, position);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input\n");
+                    break;
+            }
+
+            Console.WriteLine("Enter command: ");
+            command = int.Parse(Console.ReadLine());
         }
+        return;
     }
 }
